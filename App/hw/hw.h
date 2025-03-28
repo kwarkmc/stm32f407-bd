1bc71eb5a509def5e185d7910178713f981bdd32
kwarkmc <kwarkmc99@gmail.com>
2025-03-28 16:21:18 +0900

    Add ap, common, hw & include to build

---
+++ App/hw/hw.h
@@ -0,0 +1,9 @@
+#ifndef HW_H_
+#define HW_H_
+
+
+#include "hw_def.h"
+
+bool hwInit(void);
+
+#endif
---
+++ App/hw/hw_def.h
@@ -0,0 +1,15 @@
+#ifndef HW_DEF_H_
+#define HW_DEF_H_
+
+
+#include "main.h"
+#include "def.h"
+
+
+
+#define logPrintf printf
+
+void delay(uint32_t ms);
+uint32_t millis(void);
+
+#endif
